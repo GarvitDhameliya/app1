ccb79ba5a053cd2fb2846f9142026594e5d47533
Garvit <dhameliyagarvit@gmail.com>
2023-12-05 07:59:23 +0530

    delete api complate

--- src/App.js
+++ src/App.js
@@ -7,9 +7,10 @@ import About from "./About";
 import Contact from "./Contact";
 import Result from "./Result";
 import axios from "axios";
+import Test from "./Test";
 
 function App() {
-  const [data, setdata] = useState([]);
+  const [data, setdata] = useState();
 
   const title = useRef();
   const author = useRef();
@@ -22,53 +23,24 @@ function App() {
   }, []);
 
   function handlesubmit() {
-    const data = {
+    const maindata = {
       title: title.current.value,
       author: author.current.value,
     };
 
     // console.log(data);
 
-    axios.post("http://localhost:3001/posts", data).then((res) => {
+    axios.post("http://localhost:3001/posts", maindata).then((res) => {
       console.log(res.data);
 
-      setdata(...data, [res.data]);
+      setdata([...data, res.data]);
     });
   }
 
   console.log(data, "data");
   return (
     <div>
-      <h1>{}</h1>
-      <div>
-        {/* <input type="text" name="fname" onChange={handle} />
-        <input type="text" name="lname" onChange={handle} />
-
-        <button>Submit</button> */}
-
-        {/* <Detail /> */}
-        {/* <Navbar />
-        <Routes>
-          <Route path="/" exact element={<Home />} />
-          <Route path="/about" exact element={<About />} />
-          <Route path="/contact" exact element={<Contact />} />
-          <Route path="/:title" exact element={<Result />} />
-        </Routes> */}
-
-        <input type="text" name="title" ref={title} />
-        <input type="text" name="author" ref={author} />
-        <button onClick={handlesubmit}>submit</button>
-
-        {data?.map((val, ind) => {
-          return (
-            <>
-              <h1>{val.id}</h1>
-              <h2>{val.title}</h2>
-              <h3>{val.author}</h3>
-            </>
-          );
-        })}
-      </div>
+      <Test />
     </div>
   );
 }
